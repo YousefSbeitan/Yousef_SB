6be96793617e7971bc7a871dd20b0c0145a88afb
YousefSbeitan <sbetany380@gmail.com>
2026-05-14 09:29:31 +0300

    a6

--- src/main/java/com/tourism/booking/BookingExpirationJob.java
+++ src/main/java/com/tourism/booking/BookingExpirationJob.java
@@ -14,6 +14,7 @@ import java.util.List;
 @RequiredArgsConstructor
 public class BookingExpirationJob {
 
+
     private final BookingRepository bookingRepository;
 
     @Scheduled(fixedRate = 300000) // كل 5 دقائق
